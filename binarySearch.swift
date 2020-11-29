074b0b9b7c84d725416964fddd5373631062ad07
Stanley <stanley.traub@gmail.com>
2020-11-28 20:47:55 -0800

    Finish binarySearch

---
+++ binarySearch.swift
@@ -0,0 +1,21 @@
+func binarySearch(array: [Int], target: Int) -> Int {
+	return binarySearchHelper(array, target, 0, array.count - 1)
+}
+
+func binarySearchHelper(_ array: [Int], _ target: Int, _ leftPointer: Int, _ rightPointer: Int) -> Int {
+	if leftPointer > rightPointer {
+		return -1
+	}
+
+	let mid = (leftPointer + rightPointer) / 2
+
+	if target == array[mid] {
+		return mid
+	}
+	else if target > array[mid] {
+		return binarySearchHelper(array, target, mid + 1, rightPointer)
+	}
+	else {
+		return binarySearchHelper(array, target, leftPointer, mid - 1)
+	}
+}

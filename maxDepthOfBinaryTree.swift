a2a2e5fe3ddb97d3c934a69396963a99985c66d8
Stanley <stanley.traub@gmail.com>
2020-12-07 10:08:02 -0800

    Finish maxDepthOfBinaryTree

---
+++ maxDepthOfBinaryTree.swift
@@ -0,0 +1,22 @@
+func maxDepth(_ root: TreeNode?) -> Int {
+    guard root != nil else { return 0 }
+
+    var max = 1
+    var curNode = root
+
+    func helper(_ node: TreeNode?, _ depth: Int) {
+        if (node == nil) {
+            return
+        } else {
+            if (depth > max) {
+                max = depth
+            }
+
+            helper(node?.left, depth + 1)
+            helper(node?.right, depth + 1)
+        }
+    }
+
+    helper(curNode, max)
+    return max
+}

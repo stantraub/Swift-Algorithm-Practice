9cf6e4fe10ba4c6d983be74d5cd05e1bf5675964
Stanley <stanley.traub@gmail.com>
2020-12-23 17:00:40 -0800

    Finish max depth of a binary tree

--- maxDepthOfBinaryTree.swift
+++ maxDepthOfBinaryTree.swift
@@ -1,20 +1,21 @@
 func maxDepth(_ root: TreeNode?) -> Int {
     guard root != nil else { return 0 }
 
-    var max = 1
-    var curNode = root
+    var max = 0 
 
-    func helper(_ node: TreeNode?, _ depth: Int) {
+    func traversal(_ node: TreeNode?, _ depth: Int) {
         guard node != nil else { return }
-        
-        if (depth > max) {
+
+        if depth > max {
             max = depth
         }
 
-        helper(node?.left, depth + 1)
-        helper(node?.right, depth + 1)
+        traversal(node?.left, depth + 1)
+        traversal(node?.right, depth + 1)
+
     }
 
-    helper(curNode, max)
+    traversal(root, 1)
+
     return max
 }

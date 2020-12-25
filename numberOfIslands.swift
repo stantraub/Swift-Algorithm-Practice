c9b6b76245e58817f548fbfc3fbe60d7ca2633f4
Stanley <stanley.traub@gmail.com>
2020-12-25 11:32:45 -0800

    Finish number of islands

--- numberOfIslands.swift
+++ numberOfIslands.swift
@@ -1,3 +1,47 @@
+// BFS Solution
+
+func numIslands(_ grid: [[Character]]) -> Int {
+    let dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    var islands = 0
+    var matrix = grid
+
+    for row in matrix.indices {
+        for col in matrix[0].indices {
+            if matrix[row][col] == "1" {
+                islands += 1
+                matrix[row][col] = "0"
+                var queue = [[Int]]()
+                queue.append([row,col])
+
+                while !queue.isEmpty {
+                    let pos = queue.removeFirst()
+                    let currentRow = pos[0]
+                    let currentCol = pos[1]
+
+                    for dir in dirs {
+                        let nextRow = currentRow + dir[0]
+                        let nextCol = currentCol + dir[1]
+
+                        if nextRow < 0 || nextRow >= matrix.count || nextCol < 0 || nextCol >= matrix[0].count  {
+                            continue
+                        }
+
+                        if matrix[nextRow][nextCol] == "1" {
+                            queue.append([nextRow, nextCol])
+                            matrix[nextRow][nextCol] = "0"
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return islands
+}
+
+// DFS solution
+
 func numIslands(_ grid: [[Character]]) -> Int {
     var islands = 0
     var matrix = grid 

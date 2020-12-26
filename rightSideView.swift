1a4ceb2feb17bf66195f343694cb8212bfdd840a
Stanley <stanley.traub@gmail.com>
2020-12-26 10:34:59 -0800

    Finish right side view of tree

---
+++ rightSideView.swift
@@ -0,0 +1,25 @@
+func rightSideView(_ root: TreeNode?) -> [Int] {
+    guard let root = root else { return [] }
+
+    var res = [Int]()
+    var queue: [TreeNode] = [root]
+
+    while !queue.isEmpty {
+        var length = queue.count
+
+        for i in stride(from: 0, to: length, by: 1) {
+            let curNode = queue.removeFirst()
+            if i == length - 1 {
+                res.append(curNode.val)
+            }
+            if let left = curNode.left { queue.append(left) }
+            if let right = curNode.right { queue.append(right ) }
+
+
+        }
+
+
+    }
+
+    return res
+}

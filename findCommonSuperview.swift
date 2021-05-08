f830ae5038cdf25dc87b8ff2f58f958f03735444
Stanley <stanley.traub@gmail.com>
2021-05-08 12:25:56 -0700

    Find common superview finished

---
+++ findCommonSuperview.swift
@@ -0,0 +1,20 @@
+func getCommonSuperviews(with firstView: UIView, and secondView: UIView) -> UIView? {
+    var currentView = firstView.superview
+    var otherView = secondView.superview
+    firstView.tag = -1  // -1 means this node is visited.
+
+    while currentView != nil {
+        firstView.superview?.tag = -1  // -1 means this node is visited.
+        currentView = currentView?.superview
+    }
+
+    if secondView.tag == -1 { return secondView }
+
+    while otherView != nil {
+        // if view is visited before then return view
+        guard otherView?.tag != -1 else { return otherView }
+        otherView = otherView?.superview
+    }
+
+    return nil
+}

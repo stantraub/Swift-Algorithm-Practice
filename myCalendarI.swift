94eabb4e291c6bd31b1b8aa948fcb32866fb6bfb
Stanley <stanley.traub@gmail.com>
2021-07-18 07:35:32 -0700

    Finish myCalendarI

---
+++ myCalendarI.swift
@@ -0,0 +1,49 @@
+class Node {
+    let start: Int
+    let end: Int
+    var left: Node?
+    var right: Node?
+    
+    init(start: Int, end: Int) {
+        self.start = start
+        self.end = end
+    }
+}
+
+
+class MyCalendar {
+    
+    var root: Node?
+
+    init() {
+        
+    }
+    
+    func book(_ start: Int, _ end: Int) -> Bool {
+        if root == nil {
+            root = Node(start: start, end: end)
+            return true
+        }
+        return helper(start, end, root)
+    }
+    
+    func helper(_ start: Int, _ end: Int, _ node: Node?) -> Bool {
+        guard let node = node else { return false }
+        
+        if start >= node.end {
+            if node.right == nil {
+                node.right = Node(start: start, end: end)
+                return true
+            }
+            return helper(start, end, node.right)
+        } else if end <= node.start {
+            if node.left == nil {
+                node.left = Node(start: start, end: end)
+                return true
+            }
+            return helper(start, end, node.left)
+        } else {
+            return false 
+        }
+    }
+}
